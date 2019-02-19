ff9ee90ccac3eb12ee98f89e76bee624b65684a3
Dusty211 <kylebenz@hotmail.com>
2019-02-18 21:48:58 -0500

    all tests passing.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(names) {
+  return names.map(function(name) { return name.toLowerCase(); });
+}
+
+function nameToAttributes(names) {
+  return names.map( function(name) { return {firstName: name.split(" ")[0], lastName: name.split(" ")[1]}; });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map( function(driver) { return `${driver.name} is from ${driver.hometown}`; });
+}
